8a9bdfe9faada3fe1cc1d7bf5eaade447e47bf9e
Damien Deis <damien.deis@zenika.com>
2017-08-01 21:58:57 +0200

    chore: upgrade @angular/* dependencies

--- src/app/app.translation.module.ts
+++ src/app/app.translation.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
-import { Http, HttpModule } from '@angular/http';
+import { HttpClient } from '@angular/common/http';
 
-import { TranslateModule, TranslateLoader } from "@ngx-translate/core";
-import { TranslateHttpLoader } from "@ngx-translate/http-loader";
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { TranslateService } from '@ngx-translate/core';
 
-export function createTranslateLoader(http: Http) {
+export function createTranslateLoader(http: HttpClient) {
     return new TranslateHttpLoader(http, './assets/i18n/US/', '.json');
 }
 
@@ -13,18 +13,18 @@ const translationOptions = {
   loader: {
     provide: TranslateLoader,
     useFactory: (createTranslateLoader),
-    deps: [Http]
-  }
+    deps: [HttpClient],
+  },
 };
 
 @NgModule({
   imports: [TranslateModule.forRoot(translationOptions)],
   exports: [TranslateModule],
-  providers: [TranslateService]
+  providers: [TranslateService],
 })
 export class AppTranslationModule {
   constructor(private translate: TranslateService) {
-    translate.addLangs(["en"]);
+    translate.addLangs(['en']);
     translate.setDefaultLang('en');
     translate.use('en');
   }
